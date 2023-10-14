eeb08faf5862e725aed9b579551faf91fe4f449c
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2023-10-14 15:42:24 -0400

    chore: add recommendation algo

--- src/components/recommendation/index.jsx
+++ src/components/recommendation/index.jsx
@@ -7,7 +7,7 @@ import { AiFillClockCircle } from 'react-icons/ai'
 import { MdNumbers } from 'react-icons/md'
 import { BiSolidGroup } from 'react-icons/bi'
 import Button from '../button'
-import { colorsHex } from '../../constants'
+import { colorsHex, colors, questionOptions } from '../../constants'
 import {
   setStressLevel,
   setRootCause,
@@ -20,10 +20,152 @@ const index = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const dispatch = useDispatch()
+
   const color = useSelector((state) => state.form.color)
+  const stressLevel = useSelector((state) => state.form.stressLevel)
+  const rootCause = useSelector((state) => state.form.rootCause)
+  const meditationExperience = useSelector(
+    (state) => state.form.meditationExperience
+  )
+  const age = useSelector((state) => state.form.age)
+
+  const generateMeditationStyle = useCallback(() => {
+    switch (color) {
+      case colors.red:
+        return (
+          <span>
+            Transcendental Meditation (
+            <a
+              href="https://www.youtube.com/watch?v=fbX5eNAbpeo&pp=ygUZdHJhbnNjZW5kZW50YWwgbWVkaXRhdGlvbg%3D%3D"
+              target="_blank"
+              rel="noreferrer"
+            >
+              video explanation
+            </a>
+            )
+          </span>
+        )
+      case colors.orange:
+        return (
+          <span>
+            Zen Meditation (
+            <a
+              href="https://www.youtube.com/watch?v=aTIV9djESbY&pp=ygUOemVuIG1lZGl0YXRpb24%3D"
+              target="_blank"
+              rel="noreferrer"
+            >
+              video explanation
+            </a>
+            )
+          </span>
+        )
+      case colors.yellow:
+        return (
+          <span>
+            Guided Meditation (
+            <a
+              href="https://www.youtube.com/watch?v=Jyy0ra2WcQQ&pp=ygURZ3VpZGVkIG1lZGl0YXRpb24%3D"
+              target="_blank"
+              rel="noreferrer"
+            >
+              video explanation
+            </a>
+            )
+          </span>
+        )
+      case colors.green:
+        return (
+          <span>
+            Vipassanā Meditation (
+            <a
+              href="https://www.youtube.com/watch?v=Z7oYJZg9nOA&pp=ygUUdmlwYXNhbm5hIG1lZGl0YXRpb24%3D"
+              target="_blank"
+              rel="noreferrer"
+            >
+              video explanation
+            </a>
+            )
+          </span>
+        )
+      case colors.blue:
+        return (
+          <span>
+            Walking Meditation (
+            <a
+              href="https://www.youtube.com/watch?v=maCdzhtGgGU&pp=ygUSd2Fsa2luZyBtZWRpdGF0aW9u"
+              target="_blank"
+              rel="noreferrer"
+            >
+              video explanation
+            </a>
+            )
+          </span>
+        )
+      default:
+        return (
+          <span>
+            silent Meditation (
+            <a
+              href="https://www.youtube.com/watch?v=maCdzhtGgGU&pp=ygUSd2Fsa2luZyBtZWRpdGF0aW9u"
+              target="_blank"
+              rel="noreferrer"
+            >
+              video explanation
+            </a>
+            )
+          </span>
+        )
+    }
+  }, [color])
+
+  const generateSessionDuration = useCallback(() => {
+    let sessionDuration = 0
+
+    switch (age) {
+      case questionOptions.age[0]:
+        sessionDuration = 3
+        break
+      case questionOptions.age[1]:
+        sessionDuration = 5
+        break
+      case questionOptions.age[2]:
+        sessionDuration = 10
+        break
+      default:
+        sessionDuration = 15
+    }
+
+    if (stressLevel > 5) {
+      sessionDuration *= 2
+    }
+
+    return <span>{sessionDuration} minutes per session</span>
+  }, [])
+
+  const generateNumSessions = useCallback(() => {
+    if (
+      meditationExperience === questionOptions.meditationExperience[0] ||
+      meditationExperience === questionOptions.meditationExperience[1]
+    ) {
+      return <span>1 morning or evening</span>
+    }
+
+    return <span>1 morning and 1 evening session</span>
+  }, [])
+
+  const generateGroupStyle = useCallback(() => {
+    if (
+      rootCause === questionOptions[1] ||
+      rootCause === questionOptions[2] ||
+      rootCause === questionOptions[4]
+    ) {
+      return <span>Completed with a group</span>
+    }
+
+    return <span>Completed by yourself</span>
+  }, [])
 
   const handleResetClick = useCallback(() => {
-
     // reset form
     dispatch(setStressLevel('5'))
     dispatch(setRootCause(''))
@@ -31,7 +173,14 @@ const index = () => {
     dispatch(setAge(''))
 
     navigate(`/${location.pathname.split('/')[1]}/form`)
-  }, [])
+  }, [
+    dispatch,
+    setStressLevel,
+    setRootCause,
+    setMeditationExperience,
+    setAge,
+    location
+  ])
 
   return (
     <div id="recommendation-container">
@@ -49,17 +198,20 @@ const index = () => {
       >
         <h4>What we recommend</h4>
         <p>
-          <IoMdFlower className="recommendation-icon" /> Mediation style
+          <IoMdFlower className="recommendation-icon" />
+          {generateMeditationStyle()}
         </p>
         <p>
-          <AiFillClockCircle className="recommendation-icon" /> Session duration
+          <AiFillClockCircle className="recommendation-icon" />
+          {generateSessionDuration()}
         </p>
         <p>
-          <MdNumbers className="recommendation-icon" /> Number of sessions per
-          day
+          <MdNumbers className="recommendation-icon" />
+          {generateNumSessions()}
         </p>
         <p>
-          <BiSolidGroup className="recommendation-icon" /> Solo or group
+          <BiSolidGroup className="recommendation-icon" />
+          {generateGroupStyle()}
         </p>
       </div>
     </div>
